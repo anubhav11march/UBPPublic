c25361ab32b3b84ccee7694d1a9f7a59a3a7abe7
Anubhav Mittal <anubhav11march@gmail.com>
2020-06-08 14:33:14 +0530

    :art: Create Team Screens Designed

---
+++ app/src/main/java/com/ubptech/unitedbyplayers/CreateTeam3Fragment.java
@@ -0,0 +1,24 @@
+package com.ubptech.unitedbyplayers;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+/**
+ * Created by Kylodroid on 08-06-2020.
+ */
+public class CreateTeam3Fragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_create_team_3, container, false);
+
+        return view;
+    }
+}
--- app/src/main/java/com/ubptech/unitedbyplayers/CreateTeamActivity.java
+++ app/src/main/java/com/ubptech/unitedbyplayers/CreateTeamActivity.java
@@ -1,14 +1,50 @@
 package com.ubptech.unitedbyplayers;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class CreateTeamActivity extends AppCompatActivity {
+
+    LinearLayout nextButton;
+    static int pos = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_team);
+        getSupportActionBar().hide();
+        nextButton = (LinearLayout) findViewById(R.id.next_button);
+
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                displayFragment(++pos);
+            }
+        });
+
+        displayFragment(pos);
+    }
+
+    void displayFragment(int position){
+        Fragment fragment = null;
+        switch (position){
+            case 0: fragment = new CreateTeam1Fragment();
+                        break;
+            case 1: fragment = new CreateTeam2Fragment();
+                break;
+            case 2: fragment = new CreateTeam3Fragment();
+                break;
+        }
+        if(fragment!=null){
+            FragmentTransaction ft =getSupportFragmentManager().beginTransaction();
+            ft.replace(R.id.options, fragment);
+            ft.commitAllowingStateLoss();
+        }
+
     }
 }
