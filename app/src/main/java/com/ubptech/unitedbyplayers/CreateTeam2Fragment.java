fe3836dcbe7097c48200900af7a91a4cf315130b
Anubhav Mittal <anubhav11march@gmail.com>
2020-06-15 16:33:56 +0530

    :zap: added slider

--- app/src/main/java/com/ubptech/unitedbyplayers/CreateTeam2Fragment.java
+++ app/src/main/java/com/ubptech/unitedbyplayers/CreateTeam2Fragment.java
@@ -5,8 +5,6 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -21,9 +19,7 @@ import com.jaygoo.widget.RangeSeekBar;
 public class CreateTeam2Fragment extends Fragment {
 
     Activity activity;
-    private LinearLayout ten, fifteen, twenty, thirty, fifty;
-    private TextView tenText, fifteenText, twentyText, thirtyText, fiftyText;
-    RangeSeekBar distance, betAmount;
+    RangeSeekBar distance, betAmount, ageGroup;
 
     CreateTeam2Fragment(Activity activity){
         this.activity = activity;
@@ -38,18 +34,9 @@ public class CreateTeam2Fragment extends Fragment {
     }
 
     private void initiate(View view){
-        ten = view.findViewById(R.id.ten);
-        fifteen = view.findViewById(R.id.fifteen);
-        twenty = view.findViewById(R.id.twenty);
-        thirty = view.findViewById(R.id.thirty);
-        fifty = view.findViewById(R.id.fifty);
-        tenText = view.findViewById(R.id.ten_text);
-        fifteenText = view.findViewById(R.id.fifteen_text);
-        twentyText = view.findViewById(R.id.twenty_text);
-        thirtyText = view.findViewById(R.id.thirty_text);
-        fiftyText = view.findViewById(R.id.fifty_text);
         distance = view.findViewById(R.id.distance_seek);
         betAmount = view.findViewById(R.id.bet_seek);
+        ageGroup = view.findViewById(R.id.age_group_seek);
 
         CharSequence[] distanceArray = {"0", "50"};
         distance.setTickMarkTextArray(distanceArray);
@@ -58,6 +45,13 @@ public class CreateTeam2Fragment extends Fragment {
         betAmount.setTickMarkTextArray(betArray);
         betAmount.setIndicatorTextDecimalFormat("0");
         betAmount.setSteps(100);
+        betAmount.setStepsAutoBonding(true);
+        CharSequence[] ageGroupArray = {"13", "99"};
+        ageGroup.setTickMarkTextArray(ageGroupArray);
+        ageGroup.setIndicatorTextDecimalFormat("0");
+        ageGroup.setSteps(86);
+        ageGroup.setStepsAutoBonding(true);
+
 
         distance.setOnRangeChangedListener(new OnRangeChangedListener() {
             @Override
@@ -93,67 +87,21 @@ public class CreateTeam2Fragment extends Fragment {
             }
         });
 
-        ten.setOnClickListener(new View.OnClickListener() {
+        ageGroup.setOnRangeChangedListener(new OnRangeChangedListener() {
             @Override
-            public void onClick(View view) {
-                removeSelections();
-                ten.setBackground(getResources().getDrawable(R.drawable.blue_selected_button));
-                tenText.setTextColor(getResources().getColor(R.color.white));
-                ((CreateTeamChange) activity).setAgeGroup("10-15");
+            public void onRangeChanged(RangeSeekBar view, float leftValue, float rightValue, boolean isFromUser) {
+                ((CreateTeamChange) activity).setAgeGroup((int)leftValue + "-" + (int)rightValue);
+            }
+
+            @Override
+            public void onStartTrackingTouch(RangeSeekBar view, boolean isLeft) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(RangeSeekBar view, boolean isLeft) {
+
             }
         });
-
-        fifteen.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                removeSelections();
-                fifteen.setBackground(getResources().getDrawable(R.drawable.blue_selected_button));
-                fifteenText.setTextColor(getResources().getColor(R.color.white));
-                ((CreateTeamChange) activity).setAgeGroup("15-20");
-            }
-        });
-
-        twenty.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                removeSelections();
-                twenty.setBackground(getResources().getDrawable(R.drawable.blue_selected_button));
-                twentyText.setTextColor(getResources().getColor(R.color.white));
-                ((CreateTeamChange) activity).setAgeGroup("20-30");
-            }
-        });
-
-        thirty.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                removeSelections();
-                thirty.setBackground(getResources().getDrawable(R.drawable.blue_selected_button));
-                thirtyText.setTextColor(getResources().getColor(R.color.white));
-                ((CreateTeamChange) activity).setAgeGroup("30-40");
-            }
-        });
-
-        fifty.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                removeSelections();
-                fifty.setBackground(getResources().getDrawable(R.drawable.blue_selected_button));
-                fiftyText.setTextColor(getResources().getColor(R.color.white));
-                ((CreateTeamChange) activity).setAgeGroup("50+");
-            }
-        });
-    }
-
-    private void removeSelections(){
-        fifty.setBackground(getResources().getDrawable(R.drawable.white_selectable_button));
-        fiftyText.setTextColor(getResources().getColor(R.color.onboarding_button_blue));
-        thirty.setBackground(getResources().getDrawable(R.drawable.white_selectable_button));
-        thirtyText.setTextColor(getResources().getColor(R.color.onboarding_button_blue));
-        twenty.setBackground(getResources().getDrawable(R.drawable.white_selectable_button));
-        twentyText.setTextColor(getResources().getColor(R.color.onboarding_button_blue));
-        fifteen.setBackground(getResources().getDrawable(R.drawable.white_selectable_button));
-        fifteenText.setTextColor(getResources().getColor(R.color.onboarding_button_blue));
-        ten.setBackground(getResources().getDrawable(R.drawable.white_selectable_button));
-        tenText.setTextColor(getResources().getColor(R.color.onboarding_button_blue));
     }
 }
